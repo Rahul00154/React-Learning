beeca5a19260d65a7a85f84df2554055e4ca1b8c
Rahul00154 <103448932+Rahul00154@users.noreply.github.com>
2023-11-21 06:54:39 +0530

    useffect done

---
+++ HooksInternals/useRef/App.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function App() {
+  return <div className="App"></div>;
+}
+const root = ReactDOM.createRoot(document.getElementById("root"));
